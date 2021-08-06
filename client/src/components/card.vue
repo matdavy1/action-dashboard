2757fa812bed48f8e6e8da8bd78e761783c854f0
matdavy1 <mathew.davy@assurity.co.nz>
2021-08-06 15:47:29 +1000

    getting zip from github actions archive

--- client/src/components/card.vue
+++ client/src/components/card.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card
     class="mx-auto"
-    max-width="500"
+    max-width="1000"
   >
     <v-container fluid >
       <v-row dense>
@@ -30,35 +30,60 @@
 
 <script>
 import axios from "axios";
+import jsZip from "jszip";
 
 export default {
 
     data () {
         return {
-            workflows: null
+            workflows: null,
+            artifacts: []
         }
     },
     mounted() {
-        axios.get("/api/initialData")
-            .then((result) => { 
-                this.workflows = result.data;
-                this.workflows[0]['flex'] = 12;
-                this.workflows[1]['flex'] = 6;
-                this.workflows[2]['flex'] = 6;
-                console.log(this.workflows)
-
-            })
+      axios.get("/api/initialData")
+        .then((result) => { 
+            this.workflows = result.data.filter(workflow => workflow.repo === "my-repo");
+            this.workflows.forEach(workflow => workflow['flex'] = 6)
+            if (this.workflows.length % 2 === 1){ 
+              this.workflows[this.workflows.length - 1]['flex'] = 12
+            }
+            console.log(this.workflows)
+        }
+      )
 
     },
 
     methods: {
 
-        clicked(workflow) {
-           window.open(`https://github.com/${workflow.owner}/${workflow.repo}/actions?query=workflow%3A${workflow.workflow}`, '_blank').focus();
+      clicked(workflow) {
+        //open to github workflow
+        // window.open(`https://github.com/${workflow.owner}/${workflow.repo}/actions?query=workflow%3A${workflow.workflow}`, '_blank').focus();
 
-
-            //  this.$emit('click', this.page, event); 
-       },
+        //open to cucumber report
+        axios.get(`https://api.github.com/repos/${workflow.owner}/${workflow.repo}/actions/artifacts`,
+          {headers:{'Authorization':'token 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'}, 
+          responseType: 'json'})
+          .then(res => {
+            return res.data.artifacts.find(artifact => artifact.name === workflow.workflow).archive_download_url
+          }).then(url => {
+            axios.get(url,
+              {headers:{'Authorization':'token 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'}, 
+              responseType: 'arraybuffer'}
+            ).then(buffer => {
+            jsZip.loadAsync(buffer.data)
+            .then( zip => {
+              Object.keys(zip.files).forEach(filename => {
+                zip.files[filename].async('string')
+                .then(fileData => {
+                  window.open(fileData, '_blank').focus();
+                  console.log(fileData)      
+                })
+              })
+            })
+          })
+        })
+      },
 
       getColor(status){
         switch (status) {
